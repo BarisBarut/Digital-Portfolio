f06bf4881a1686fe614c1f22b09e7f95c42fa2a7
BarisBarut <125616721+BarisBarut@users.noreply.github.com>
2025-01-23 06:04:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// Function to show the reflective blogs section
+// Function to show the reflective blogs section and hide the homepage content
 function showReflectiveBlogs() {
   document.getElementById('reflective_blogs').style.display = 'block';
+  document.querySelector('.about-me').style.display = 'none'; // Hide 'About Me' section
 }
 
 // Add event listener to the Reflective Blogs tab to show the section when clicked
